1b7a28ad5fe64235f0bd15d507e6c269797d29cc
datsosweet <109160530+datsosweet@users.noreply.github.com>
2022-08-11 14:56:57 +0700

    Update scene.py

--- scene.py
+++ scene.py
@@ -3,12 +3,7 @@ from manim import *
 class Testing(Scene):
     def construct(self):
 
-        name = Tex("Dinh Tien Dat").to_edge(UR, buff = 0.5)
-        sq = Square(side_length = 0.5, fill_color = RED, fill_opacity = 0.5).shift(
-            LEFT * 3)
         tri = Triangle().scale(3).to_edge(DL)
 
-        self.play(Write(name))
-        self.play(DrawBorderThenFill(sq), runtime = 3)
         self.play(Create(tri))
         self.wait()
